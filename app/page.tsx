92a43325b9aeb6b2c400f63b8b4541e6823e74fe
vinisha-Krack-ai <vinisha.gunturu@krack-ai.com>
2026-08-12 15:05:42 +0530

    created sample component

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,14 @@
 
 import Button from "@/components/Button/Button";
 import { useCounter } from "@/hooks/useCounter";
+import TestComponent from "@/components/TestComponent";
 
 export default function Home() {
   const { count, increment, decrement, reset } = useCounter();
 
   return (
+    <>
+    <TestComponent/>
     <main className="flex min-h-screen flex-col items-center justify-center gap-6">
       <h1 className="text-4xl font-bold">
         Kiran Reddy Boys Hostel
@@ -22,5 +25,6 @@ export default function Home() {
         <Button label="+" onClick={increment} />
       </div>
     </main>
+    </>
   );
 }
---
+++ components/TestComponent.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TestComponent() {
+  return (
+    <div className='text-5xl text-center font-bold mt-4'>Welcome to Hostel Webpage</div>
+  )
+}
